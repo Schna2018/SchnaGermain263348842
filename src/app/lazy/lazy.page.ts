7eaea75639f68db6f11b7996802fc7af65ac67aa
Schna2018 <163787973+Schna2018@users.noreply.github.com>
2024-10-26 12:54:00 -0300

    Update lazy.page.ts

--- src/app/lazy/lazy.page.ts
+++ src/app/lazy/lazy.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SchnaService } from './schna.service';
-import { JsonpInterceptor } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-lazy',
